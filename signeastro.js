c316e48eca50c651d42e0fe06fc7d624846b50ed
rafmiqgus <rafaelmiqgus@gmail.com>
2024-03-01 00:09:13 +0100

    commit

--- signeastro.js
+++ signeastro.js
@@ -140,25 +140,32 @@ document.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOM loaded...");
 
   const header_container = document.querySelector(".header_container");
-  const arrow_up = document.getElementById("arrow_up");
   const window_height = window.innerHeight;
-
   let headerContainerHeight = header_container.getBoundingClientRect().height;
-
   const updateHeaderHeight = () => {
     headerContainerHeight = header_container.getBoundingClientRect().height;
     console.log("Updated header height:", headerContainerHeight);
+    console.log("Window height / 2:", window_height / 2);
   };
+
+  const arrow_up = document.querySelector(".arrow_up");
 
   window.addEventListener("wheel", function (event) {
     updateHeaderHeight();
     if (event.deltaY > 0 && headerContainerHeight > window.innerHeight / 2) {
       header_container.classList.remove("HeaderContainerUpAnimation");
       header_container.classList.add("HeaderContainerDownAnimation");
+      arrow_up.classList.remove("ArrowFadeOut");
+      arrow_up.classList.add("ArrowFadeIn");
       console.log("scroll down");
-    } else if (event.deltaY < 0 && headerContainerHeight <= window.innerHeight / 2) {
+    } else if (
+      event.deltaY < 0 &&
+      headerContainerHeight <= window.innerHeight / 2
+    ) {
       header_container.classList.remove("HeaderContainerDownAnimation");
       header_container.classList.add("HeaderContainerUpAnimation");
+      arrow_up.classList.remove("ArrowFadeIn");
+      arrow_up.classList.add("ArrowFadeOut");
       console.log("scroll up");
     }
   });
